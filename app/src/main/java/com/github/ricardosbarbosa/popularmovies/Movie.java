0cc3fe1b7101d0d2938fe319b78411554d36212b
Ricardo Barbosa <rbrico@gmail.com>
2017-01-25 12:15:31 -0300

    Initial commit

---
+++ app/src/main/java/com/github/ricardosbarbosa/popularmovies/Movie.java
@@ -0,0 +1,73 @@
+package com.github.ricardosbarbosa.popularmovies;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable{
+
+    final String baseURL = "http://image.tmdb.org/t/p/";
+    final String defaultSize = "w185";
+
+    public final Integer id;
+    public final String overview;
+    public final String title;
+    public final Double rating;
+    public final String posterPath;
+    public final String releaseDate;
+
+    public Movie(Integer id, String overview, String title, Double rating, String posterPath, String releaseDate) {
+        this.id = id;
+        this.overview = overview;
+        this.title = title;
+        this.rating = rating;
+        this.posterPath = posterPath;
+        this.releaseDate = releaseDate;
+    }
+
+    private Movie(Parcel in){
+        this.id = in.readInt();
+        this.overview = in.readString();
+        this.title = in.readString();
+        this.rating = in.readDouble();
+        this.posterPath = in.readString();
+        this.releaseDate = in.readString();
+    }
+
+    public String getFullPosterPath() {
+        return baseURL + defaultSize + posterPath;
+
+    }
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(overview);
+        dest.writeString(title);
+        dest.writeDouble(rating);
+        dest.writeString(posterPath);
+        dest.writeString(releaseDate);
+
+    }
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel parcel) {
+            return new Movie(parcel);
+        }
+
+        @Override
+        public Movie[] newArray(int i) {
+            return new Movie[i];
+        }
+
+    };
+}
---
+++ app/src/main/java/com/github/ricardosbarbosa/popularmovies/MovieDetailFragment.java
@@ -0,0 +1,68 @@
+package com.github.ricardosbarbosa.popularmovies;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * A fragment representing a single Movie detail screen.
+ * This fragment is either contained in a {@link MovieListActivity}
+ * in two-pane mode (on tablets) or a {@link MovieDetailActivity}
+ * on handsets.
+ */
+public class MovieDetailFragment extends Fragment {
+    public static final String ARG_MOVIE_ID = "id";
+
+    private Movie movie;
+
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public MovieDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_MOVIE_ID)) {
+            movie = (Movie) getArguments().getParcelable(ARG_MOVIE_ID);
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(movie.title);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.movie_detail, container, false);
+
+        if (movie != null) {
+            ((TextView) rootView.findViewById(R.id.movie_title)).setText(movie.title);
+            ((TextView) rootView.findViewById(R.id.movie_rate)).setText(movie.rating.toString());
+            ((TextView) rootView.findViewById(R.id.movie_overview)).setText(movie.overview);
+            ((TextView) rootView.findViewById(R.id.movie_release_date)).setText(movie.releaseDate);
+            ImageView imageView =((ImageView) rootView.findViewById(R.id.movie_poster));
+            Picasso.with(getContext()).load(movie.getFullPosterPath()).into(imageView);
+        }
+
+        return rootView;
+    }
+
+
+
+}
